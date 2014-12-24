ebb298a1fa351c9288c8a0f4877f076cf1826dc9
Richard Stebbing <richie.stebbing@gmail.com>
2014-12-25 07:48:33 +1300

    Add docstrings to uniform_bspline.py

--- uniform_bspline.py
+++ uniform_bspline.py
@@ -17,6 +17,26 @@ __all__ = ['B',
 # B
 @sp.cacheit
 def B(i, k, x):
+    """Symbolically evaluate the B-spline basis with uniform knot spacing
+    (http://en.wikipedia.org/wiki/B-spline).
+
+    Parameters
+    ----------
+    i : int
+        Translation (delay) of the basis on the x-axis.
+
+    k: int
+        Order of the B-spline.
+
+    x : sp.Symbol
+        Variable.
+
+    Returns
+    -------
+    b : list of sp.Expr, len = k + i
+        `b[i]` is the expression (in `x`) for the basis over the half-open
+        interval [i, i + 1).
+    """
     if k < 1:
         raise ValueError('k < 1 (= {})'.format(k))
     if i < 0:
@@ -43,6 +63,22 @@ def B(i, k, x):
 # basis_functions
 @sp.cacheit
 def basis_functions(d, x):
+    """Symbolically evaluate the uniform B-spline basis functions.
+
+    Parameters
+    ----------
+    d : int
+        The degree of the uniform B-spline.
+
+    x : sp.Symbol
+        Interpolation parameter.
+
+    Returns
+    -------
+    b : list of sp.Expr, len = d + 1
+        List of B-spline basis functions, where `b[i]` is the interpolating
+        expression over the unit interval.
+    """
     if d < 0:
         raise ValueError('d < 0 (= {})'.format(d))
 
@@ -63,6 +99,24 @@ UNIFORM_BSPLINE_TEMPLATE = """def {func_name}(t):
 """
 @sp.cacheit
 def uniform_bspline_basis(d, p=0):
+    """Generate a "Numpy friendly" function to facilitate fast evaluation of
+    uniform B-spline basis functions.
+
+    Parameters
+    ----------
+    d : int
+        The degree of the uniform B-spline.
+
+    p : optional, int
+        The order of the derivative with respect to the interpolation
+        parameter.
+
+    Returns
+    -------
+    uniform_bspline_basis_d : function
+        "Numpy friendly" function to evaluate the uniform B-spline
+        interpolation components.
+    """
     t = sp.Symbol('t')
     b = basis_functions(d, t)
     for i in range(p):
@@ -81,6 +135,26 @@ def uniform_bspline_basis(d, p=0):
 
 # Contour
 class Contour(object):
+    """Contour
+
+    Class to facilitate evaluation of points on, and derivatives of, a uniform
+    B-spline.
+
+    Parameters
+    ----------
+    degree : int
+        The degree of the uniform B-spline.
+
+    num_control_points : int
+        The number of control points for the B-spline.
+
+    dim : int
+        Number of dimensions of each control point (typically 2 or 3).
+
+    is_closed optional, bool
+        True if the contour is closed, False otherwise.
+    """
+
     def __init__(self, degree, num_control_points, dim, is_closed=False):
         if degree < 1:
             raise ValueError('degree < 1 (= {})'.format(degree))
@@ -100,15 +174,73 @@ class Contour(object):
         self._Wt = uniform_bspline_basis(degree, 1)
 
     def uniform_parameterisation(self, N):
+        """Generate a vector of coordinates `u` which parameterise linearly
+        distributed points on the contour.
+
+        Parameters
+        ----------
+        N : int
+            The number of points.
+
+        Returns
+        -------
+        u : float, np.ndarray of shape = (N,)
+            The vector of contour coordinates.
+        """
         return np.linspace(0.0, self.num_segments, N, endpoint=False)
 
     def M(self, u, X):
+        """Evaluate points on the contour.
+
+        Parameters
+        ----------
+        u : float, array_like of shape = (N,)
+            The vector of contour coordinates.
+
+        X : float, array_like of shape = (num_control_points, dim)
+            The matrix of control point positions.
+
+        Returns
+        -------
+        M : float, np.ndarray of shape = (N, dim)
+            The matrix of evaluated positions.
+        """
         return self._f(self._W, u, X)
 
     def Mu(self, u, X):
+        """Evaluate first derivatives with respect to `u` on the contour.
+
+        Parameters
+        ----------
+        u : float, array_like of shape = (N,)
+            The vector of contour coordinates.
+
+        X : float, array_like of shape = (num_control_points, dim)
+            The matrix of control point positions.
+
+        Returns
+        -------
+        Mu : float, np.ndarray of shape = (N, dim)
+            The matrix of evaluated first derivatives.
+        """
         return self._f(self._Wt, u, X)
 
     def MX(self, u):
+        """Evaluate first derivatives with respect to `X` on the contour.
+
+        Parameters
+        ----------
+        u : float, array_like of shape = (N,)
+            The vector of contour coordinates.
+
+        Returns
+        -------
+        J : float, np.ndarray of shape = (N * dim, num_control_points * dim)
+            The full Jacobian of first derivatives.
+            `J[dim * i + k, dim * j + l]` is the derivative of the `k`th
+            component of the `i`th point with respect to component `l` of the
+            `j`th control point.
+        """
         u, s, t = self._u_to_s_t(u)
         (N,) = u.shape
 
@@ -136,6 +268,8 @@ class Contour(object):
         return R
 
     def _u_to_s_t(self, u):
+        """Ensure the contour coordinate `u` is valid and translate it to a
+        segment index `s` and segment coordinate `t`."""
         u = np.atleast_1d(u)
         (N,) = u.shape
 
@@ -149,5 +283,6 @@ class Contour(object):
         return u, s, t
 
     def _i(self, s):
+        """Provide the control point indices for segment `s`."""
         return map(lambda i: i % self.num_control_points,
                    range(s, s + self._degree + 1))
