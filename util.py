f841a816b392b5561853da297efc0637f66f8cdb
Richard Stebbing <richie.stebbing@gmail.com>
2014-12-25 19:28:50 +1300

    Add docstring to `raise_if_not_shape` in util.py

--- util.py
+++ util.py
@@ -2,5 +2,7 @@
 
 # raise_if_not_shape
 def raise_if_not_shape(name, A, shape):
+    """Raise a `ValueError` if the np.ndarray `A` does not have dimensions
+    `shape`."""
     if A.shape != shape:
         raise ValueError('{}.shape != {}'.format(name, shape))
